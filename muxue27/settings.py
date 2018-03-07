8fe86bc8d9fc6dc074e500ef5b42e394b5fd95de
jihuixin <jihuixin@xjgreat.com>
2018-03-07 22:12:20 +0800

    配置静态资源访问路径

--- muxue27/settings.py
+++ muxue27/settings.py
@@ -129,4 +129,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ("css", os.path.join(STATIC_ROOT, 'css')),
+    ("image", os.path.join(STATIC_ROOT, 'image')),
+)
+
+print (STATIC_ROOT)
+print (STATICFILES_DIRS)
+print (BASE_DIR)
